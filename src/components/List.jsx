91bbad1a6df37699e13ff005624a8a982df2bafa
alex lima <alexlimaequise5@gmail.com>
2024-04-20 13:52:29 -0400

    pregunta 3

---
+++ src/components/List.jsx
@@ -0,0 +1,10 @@
+export const List = (props) => {
+    const { list } = props; 
+    return (
+        <ul>
+            {list.map((item, index) => (
+                <li key={index}>{item}</li>
+            ))}
+        </ul>
+    );
+};
